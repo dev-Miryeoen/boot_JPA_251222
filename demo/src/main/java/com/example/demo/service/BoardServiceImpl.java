ec5bdaeedc3a2d3783a28cd72c2ed790cfd37c0e
sangwon <derange63@gmail.com>
2025-12-29 17:26:36 +0900

    251229

--- demo/src/main/java/com/example/demo/service/BoardServiceImpl.java
+++ demo/src/main/java/com/example/demo/service/BoardServiceImpl.java
@@ -200,13 +200,23 @@ public class BoardServiceImpl implements BoardService{
     }
 
 
+//    @Override
+//    public Page<BoardDTO> getList(int pageNo) {
+//        // limit 시작번지, 개수 => 번지는 0부터 시작
+//        // pageNo = 1 => limit 0,10
+//        Pageable pageable = PageRequest.of(pageNo-1, 10, Sort.by("bno").descending());
+//        Page<Board> pageList = boardRepository.findAll(pageable);
+//        log.info(">>> pageList >> {}", pageList);
+//        Page<BoardDTO> boardDTOPage = pageList.map(this::convertEntityToDTO);
+//        return boardDTOPage;
+//    }
+
     @Override
-    public Page<BoardDTO> getList(int pageNo) {
-        // limit 시작번지, 개수 => 번지는 0부터 시작
-        // pageNo = 1 => limit 0,10
+    public Page<BoardDTO> getList(int pageNo, String type, String keyword) {
         Pageable pageable = PageRequest.of(pageNo-1, 10, Sort.by("bno").descending());
-        Page<Board> pageList = boardRepository.findAll(pageable);
-        log.info(">>> pageList >> {}", pageList);
+        // type과 keyword에 일치하는 리스트를 리턴
+        Page<Board> pageList = boardRepository.searchBoard(type, keyword, pageable);
+
         Page<BoardDTO> boardDTOPage = pageList.map(this::convertEntityToDTO);
         return boardDTOPage;
     }
